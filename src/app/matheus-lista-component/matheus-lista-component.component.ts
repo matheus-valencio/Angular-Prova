4545e9782fd9da2548ba4cb29364f567bfd9641f
matheus-valencio <matheus.valencio@live.com>
2022-04-08 20:22:28 -0300

    3 files modified

--- src/app/matheus-lista-component/matheus-lista-component.component.ts
+++ src/app/matheus-lista-component/matheus-lista-component.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatheusDisciplinasServiceService } from '../matheus-disciplinas-service.service';
 
 @Component({
   selector: 'app-matheus-lista-component',
   templateUrl: './matheus-lista-component.component.html',
-  styleUrls: ['./matheus-lista-component.component.css']
+  styleUrls: ['./matheus-lista-component.component.css'],
 })
 export class MatheusListaComponentComponent implements OnInit {
+  constructor(public disciplinas: MatheusDisciplinasServiceService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
