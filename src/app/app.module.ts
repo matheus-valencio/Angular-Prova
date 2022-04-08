4e8582011d5b48f5df3c17655f54d56191140920
matheus-valencio <matheus.valencio@live.com>
2022-04-08 19:54:30 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { MatheusComponentComponent } from './matheus-component/matheus-component.component';
+import { MatheusListaComponentComponent } from './matheus-lista-component/matheus-lista-component.component';
+import { MatheusDetalhesComponentComponent } from './matheus-detalhes-component/matheus-detalhes-component.component';
+import { MatheusDisciplinasServiceService } from './matheus-disciplinas-service.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: MatheusComponentComponent },
+      { path: 'lista', component: MatheusListaComponentComponent },
+      { path: 'lista/:index', component: MatheusComponentComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MatheusComponentComponent,
+    MatheusListaComponentComponent,
+    MatheusDetalhesComponentComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [MatheusDisciplinasServiceService],
 })
-export class AppModule { }
+export class AppModule {}
